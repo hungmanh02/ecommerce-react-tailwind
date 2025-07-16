f5930df9a7040d95c583573d0afaf03e771dcafe
hungmanh02 <domanh462@gmail.com>
2025-07-16 14:47:41 +0700

    slider

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import Slider from "react-slick";
 import Image1 from "../../assets/hero/women.png";
 import Image2 from "../../assets/hero/shopping.png";
 import Image3 from "../../assets/hero/sale.png";
 const ImageList = [
     {
         id:1,
-        img: Image1 ,
+        image: Image1 ,
         title: "Upto 50% off on all Men's Wear",
         description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fuga quo, dolorem quis corrupti illum alias molestias placeat dicta expedita quidem laudantium eligendi! Architecto, ratione alias esse reiciendis maxime repellendus!",
     },
     {
         id:2,
-        img: Image2 ,
+        image: Image2 ,
         title: "30% off on all Shopping",
         description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fuga quo, dolorem quis corrupti illum alias molestias placeat dicta expedita quidem laudantium eligendi! Architecto, ratione alias esse reiciendis maxime repellendus!",
     },
     {
         id:3,
-        img: Image3,
+        image: Image3,
         title: "70% off on all Products Sale",
         description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fuga quo, dolorem quis corrupti illum alias molestias placeat dicta expedita quidem laudantium eligendi! Architecto, ratione alias esse reiciendis maxime repellendus!",
@@ -28,6 +29,18 @@ const ImageList = [
 
 ];
 const Hero = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed :4000,
+    cssEase:"ease-in-out",
+    pauseOnHover:false,
+    pauseOnFocus:true,
+  };
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center duration-200'>
       {/* background pattern */}
@@ -36,19 +49,26 @@ const Hero = () => {
       </div>
       {/* hero section */}
       <div className='container pb-8 sm:pb-0'>
+          <Slider  {...settings}>
+            {ImageList.map((data)=>(
         <div>
-            <div className='grid grid-cols-1 sm:grid-cols-2'>
-                {/* text content section */}
-                <div>
-                    <h1 className='text-5xl sm:text-6xl lg:text-7xl'>Lorem ipsum dolor sit.</h1>
-                    <p className='text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, sint?</p>
-                    <div><button className='bg-gradient-to-r from-[#fea928] to-[#ed8900] hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button></div>
+          <div className='grid grid-cols-1 sm:grid-cols-2'>
+              {/* text content section */}
+              <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                  <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                  <p className='text-sm'>{data.description}</p>
+                  <div><button className='bg-gradient-to-r from-[#fea928] to-[#ed8900] hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button></div>
+              </div>
+              {/* imgae section */}
+              <div className='order-1 sm:order-2'>
+                <div className='relative z-10'>
+                  <img src={data.image} alt=""  className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-110 object-contain mx-auto'/>
                 </div>
-                {/* text content section */}
-
-
-            </div>
+              </div>
+          </div>
         </div>
+              ))}
+          </Slider>
       </div>
     </div>
   )
